5d3c2706d720c86dea36d3f297171e2d974d6acb
amin <liminghui168@outlook.com>
2019-12-24 11:48:55 +0800

    模块设计 URL解析和数据请求

--- src/query/App.js
+++ src/query/App.js
@@ -1,10 +1,165 @@
+import React, { useCallback, useEffect } from 'react'
 import { connect } from 'react-redux'
+import URI from 'urijs'
+import dayjs from 'dayjs'
+import { h0 } from '../common/fp'
 
 import './App.css'
 
-function App(props) {}
+import Header from '../common/Header'
+import Nav from '../common/Nav'
+import List from './List'
+import Bottom from './Bottom'
+
+import {
+    setFrom,
+    setTo,
+    setDepartDate,
+    setHighSpeed,
+    setSearchParsed,
+    setTrainList,
+    setTicketTypes,
+    setTrainTypes,
+    setDepartStations,
+    setArriveStations
+} from './actions'
+
+function App(props) {
+    const {
+        from,
+        to,
+        departDate,
+        highSpeed,
+        searchParsed,
+        orderType,
+        onlyTickets,
+        checkedTicketTypes,
+        checkedTrainTypes,
+        checkedDepartStations,
+        checkedArriveStations,
+        departTimeStart,
+        departTimeEnd,
+        arriveTimeStart,
+        arriveTimeEnd,
+        dispatch
+    } = props
+
+    useEffect(() => {
+        const queries = URI.parseQuery(window.location.search)
+
+        const { from, to, date, highSpeed } = queries
+
+        dispatch(setFrom(from))
+        dispatch(setTo(to))
+        // valueOf 返回unix时间戳
+        dispatch(setDepartDate(h0(dayjs(date).valueOf())))
+        dispatch(setHighSpeed(highSpeed === 'true'))
+
+        dispatch(setSearchParsed(true))
+
+        // eslint-disable-next-line
+    }, [])
+
+    useEffect(() => {
+        if (!searchParsed) {
+            return
+        }
+
+        const url = new URI('/rest/query')
+            .setSearch('from', from)
+            .setSearch('to', to)
+            .setSearch('date', dayjs(departDate).format('YYYY-MM-DD'))
+            .setSearch('highSpeed', highSpeed)
+            .setSearch('orderType', orderType)
+            .setSearch('onlyTickets', onlyTickets)
+            .setSearch(
+                'checkedTicketTypes',
+                Object.keys(checkedTicketTypes).join() //因为checkedTicketTypes是对象，这里将key转化为字符串
+            )
+            .setSearch(
+                'checkedTrainTypes',
+                Object.keys(checkedTrainTypes).join()
+            )
+            .setSearch(
+                'checkedDepartStations',
+                Object.keys(checkedDepartStations).join()
+            )
+            .setSearch(
+                'checkedArriveStations',
+                Object.keys(checkedArriveStations).join()
+            )
+            .setSearch('departTimeStart', departTimeStart)
+            .setSearch('departTimeEnd', departTimeEnd)
+            .setSearch('arriveTimeStart', arriveTimeStart)
+            .setSearch('arriveTimeEnd', arriveTimeEnd)
+            .toString()
+
+        fetch(url)
+            .then(res => res.json())
+            .then(result => {
+                const {
+                    dataMap: {
+                        directTrainInfo: {
+                            trains, //车次列表
+                            filter: {
+                                ticketType,
+                                trainType,
+                                depStation,
+                                arrStation
+                            } //筛选条件
+                        }
+                    }
+                } = result
+
+                dispatch(setTrainList(trains))
+                dispatch(setTicketTypes(ticketType))
+                dispatch(setTrainTypes(trainType))
+                dispatch(setDepartStations(depStation))
+                dispatch(setArriveStations(arrStation))
+            })
+    }, [
+        from,
+        to,
+        departDate,
+        highSpeed,
+        searchParsed,
+        orderType,
+        onlyTickets,
+        checkedTicketTypes,
+        checkedTrainTypes,
+        checkedDepartStations,
+        checkedArriveStations,
+        departTimeStart,
+        departTimeEnd,
+        arriveTimeStart,
+        arriveTimeEnd
+    ])
+
+    const onBack = useCallback(() => {
+        window.history.back()
+    }, [])
+
+    if (!searchParsed) {
+        return <div>loading...</div>
+    }
+
+    return (
+        <div>
+            <div className="header-wrapper">
+                <Header onBack={onBack} title={`${from} ⇀ ${to}`} />
+            </div>
+            <Nav />
+            <List />
+            <Bottom />
+        </div>
+    )
+}
 
 export default connect(
-    function mapStateToProps(state) {},
-    function mapDispatchToProps(dispatch) {}
+    function mapStateToProps(state) {
+        return state
+    },
+    function mapDispatchToProps(dispatch) {
+        return { dispatch }
+    }
 )(App)
---
+++ src/query/constant.js
@@ -0,0 +1,11 @@
+/**
+ * 出发早到晚
+ * @type {number}
+ */
+export const ORDER_DEPART = 1
+
+/**
+ * 耗时短到长
+ * @type {number}
+ */
+export const ORDER_DURATION = 2
