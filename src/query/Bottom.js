da1245e06eb7317d5660d652290ec12da8b075bc
amin <liminghui168@outlook.com>
2019-12-25 15:26:53 +0800

    筛选浮层，用useState来缓存筛选条件的数据

--- src/query/Bottom.js
+++ src/query/Bottom.js
@@ -1,9 +1,190 @@
-import React from 'react'
+import React, { memo, useCallback, useState } from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 
 import './Bottom.css'
 import { ORDER_DEPART } from './constant'
+
+const Filter = memo(function Filter(props) {
+    const { name, checked, value, toggle } = props
+
+    return (
+        <li className={classnames({ checked })} onClick={() => toggle(value)}>
+            {name}
+        </li>
+    )
+})
+
+Filter.propTypes = {
+    name: PropTypes.string.isRequired,
+    checked: PropTypes.bool.isRequired,
+    value: PropTypes.string.isRequired,
+    toggle: PropTypes.func.isRequired
+}
+
+const Option = memo(function Option(props) {
+    const { title, options, checkedMap, update } = props
+
+    const toggle = useCallback(
+        value => {
+            const newCheckedMap = { ...checkedMap }
+
+            if (value in checkedMap) {
+                delete newCheckedMap[value]
+            } else {
+                newCheckedMap[value] = true
+            }
+
+            update(newCheckedMap)
+        },
+        [checkedMap, update]
+    )
+
+    return (
+        <div className="option">
+            <h3>{title}</h3>
+            <ul>
+                {options.map(option => {
+                    return (
+                        <Filter
+                            key={option.value}
+                            {...option}
+                            checked={option.value in checkedMap}
+                            toggle={toggle}
+                        />
+                    )
+                })}
+            </ul>
+        </div>
+    )
+})
+
+Option.propTypes = {
+    title: PropTypes.string.isRequired,
+    options: PropTypes.array.isRequired,
+    checkedMap: PropTypes.object.isRequired,
+    update: PropTypes.func.isRequired
+}
+
+const BottomModal = memo(function BottomModal(props) {
+    const {
+        ticketTypes,
+        trainTypes,
+        departStations,
+        arriveStations,
+        checkedTicketTypes,
+        checkedTrainTypes,
+        checkedDepartStations,
+        checkedArriveStations,
+        departTimeStart,
+        departTimeEnd,
+        arriveTimeStart,
+        arriveTimeEnd
+    } = props
+
+    // 由于checkedTicketTypes 计算可能会消耗性能，所以用延迟加载来初始化
+    // 仅在初始渲染时执行一次
+    const [localCheckedTicketTypes, setLocalCheckedTicketTypes] = useState(
+        () => {
+            return {
+                ...checkedTicketTypes
+            }
+        }
+    )
+
+    const [localCheckedTrainTypes, setLocalCheckedTrainTypes] = useState(() => {
+        return {
+            ...checkedTrainTypes
+        }
+    })
+
+    const [
+        localCheckedDepartStations,
+        setLocalCheckedDepartStations
+    ] = useState(() => {
+        return {
+            ...checkedDepartStations
+        }
+    })
+
+    const [
+        localCheckedArriveStations,
+        setLocalCheckedArriveStations
+    ] = useState(() => {
+        return {
+            ...checkedArriveStations
+        }
+    })
+
+    const [localDepartTimeStart, setLocalDepartTimeStart] = useState(
+        departTimeStart
+    )
+    const [localDepartTimeEnd, setLocalDepartTimeEnd] = useState(departTimeEnd)
+    const [localArriveTimeStart, setLocalArriveTimeStart] = useState(
+        arriveTimeStart
+    )
+    const [localArriveTimeEnd, setLocalArriveTimeEnd] = useState(arriveTimeEnd)
+
+    const optionGroup = [
+        {
+            title: '坐席类型',
+            options: ticketTypes,
+            checkedMap: localCheckedTicketTypes,
+            update: setLocalCheckedTicketTypes
+        },
+        {
+            title: '车次类型',
+            options: trainTypes,
+            checkedMap: localCheckedTrainTypes,
+            update: setLocalCheckedTrainTypes
+        },
+        {
+            title: '出发车站',
+            options: departStations,
+            checkedMap: localCheckedDepartStations,
+            update: setLocalCheckedDepartStations
+        },
+        {
+            title: '到达车站',
+            options: arriveStations,
+            checkedMap: localCheckedArriveStations,
+            update: setLocalCheckedArriveStations
+        }
+    ]
+
+    return (
+        <div className="bottom-modal">
+            <div className="bottom-dialog">
+                <div className="bottom-dialog-content">
+                    <div className="title">
+                        <span className="reset">重置</span>
+                        <span className="ok">确定</span>
+                    </div>
+                    <div className="options">
+                        {optionGroup.map(group => (
+                            <Option key={group.title} {...group} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+})
+
+BottomModal.propTypes = {
+    ticketTypes: PropTypes.array.isRequired,
+    trainTypes: PropTypes.array.isRequired,
+    departStations: PropTypes.array.isRequired,
+    arriveStations: PropTypes.array.isRequired,
+    checkedTicketTypes: PropTypes.object.isRequired,
+    checkedTrainTypes: PropTypes.object.isRequired,
+    checkedDepartStations: PropTypes.object.isRequired,
+    checkedArriveStations: PropTypes.object.isRequired,
+    departTimeStart: PropTypes.number.isRequired,
+    departTimeEnd: PropTypes.number.isRequired,
+    arriveTimeStart: PropTypes.number.isRequired,
+    arriveTimeEnd: PropTypes.number.isRequired
+}
 
 export default function Bottom(props) {
     const {
@@ -14,7 +195,19 @@ export default function Bottom(props) {
         toggleOrderType,
         toggleHighSpeed,
         toggleOnlyTickets,
-        toggleIsFiltersVisible
+        toggleIsFiltersVisible,
+        ticketTypes,
+        trainTypes,
+        departStations,
+        arriveStations,
+        checkedTicketTypes,
+        checkedTrainTypes,
+        checkedDepartStations,
+        checkedArriveStations,
+        departTimeStart,
+        departTimeEnd,
+        arriveTimeStart,
+        arriveTimeEnd
     } = props
 
     return (
@@ -48,6 +241,22 @@ export default function Bottom(props) {
                     综合筛选
                 </span>
             </div>
+            {isFiltersVisible && (
+                <BottomModal
+                    ticketTypes={ticketTypes}
+                    trainTypes={trainTypes}
+                    departStations={departStations}
+                    arriveStations={arriveStations}
+                    checkedTicketTypes={checkedTicketTypes}
+                    checkedTrainTypes={checkedTrainTypes}
+                    checkedDepartStations={checkedDepartStations}
+                    checkedArriveStations={checkedArriveStations}
+                    departTimeStart={departTimeStart}
+                    departTimeEnd={departTimeEnd}
+                    arriveTimeStart={arriveTimeStart}
+                    arriveTimeEnd={arriveTimeEnd}
+                />
+            )}
         </div>
     )
 }
@@ -60,5 +269,25 @@ Bottom.propTypes = {
     toggleOrderType: PropTypes.func.isRequired,
     toggleHighSpeed: PropTypes.func.isRequired,
     toggleOnlyTickets: PropTypes.func.isRequired,
-    toggleIsFiltersVisible: PropTypes.func.isRequired
+    toggleIsFiltersVisible: PropTypes.func.isRequired,
+    ticketTypes: PropTypes.array.isRequired,
+    trainTypes: PropTypes.array.isRequired,
+    departStations: PropTypes.array.isRequired,
+    arriveStations: PropTypes.array.isRequired,
+    checkedTicketTypes: PropTypes.object.isRequired,
+    checkedTrainTypes: PropTypes.object.isRequired,
+    checkedDepartStations: PropTypes.object.isRequired,
+    checkedArriveStations: PropTypes.object.isRequired,
+    departTimeStart: PropTypes.number.isRequired,
+    departTimeEnd: PropTypes.number.isRequired,
+    arriveTimeStart: PropTypes.number.isRequired,
+    arriveTimeEnd: PropTypes.number.isRequired,
+    setCheckedTicketTypes: PropTypes.func.isRequired,
+    setCheckedTrainTypes: PropTypes.func.isRequired,
+    setCheckedDepartStations: PropTypes.func.isRequired,
+    setCheckedArriveStations: PropTypes.func.isRequired,
+    setDepartTimeStart: PropTypes.func.isRequired,
+    setDepartTimeEnd: PropTypes.func.isRequired,
+    setArriveTimeStart: PropTypes.func.isRequired,
+    setArriveTimeEnd: PropTypes.func.isRequired
 }
