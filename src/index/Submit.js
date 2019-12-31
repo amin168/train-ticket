0652a7998d488a8f76c2cfd32ef5b088bd18d844
amin <liminghui168@outlook.com>
2019-12-31 11:24:31 +0800

    tabWith: 4 -> 2

--- src/index/Submit.js
+++ src/index/Submit.js
@@ -4,11 +4,11 @@ import './Submit.css'
 import React, { memo } from 'react'
 
 export default memo(function Submit() {
-    return (
-        <div className="submit">
-            <button type="submit" className="submit-button">
-                搜索
-            </button>
-        </div>
-    )
+  return (
+    <div className="submit">
+      <button type="submit" className="submit-button">
+        搜索
+      </button>
+    </div>
+  )
 })
